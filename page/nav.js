3fdcb761e5b06b379ed46324f7bcf6396dae5dd2
mas8074 <43452057+mas8074@users.noreply.github.com>
2022-09-29 15:17:32 -0400

    added css

--- page/nav.js
+++ page/nav.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$("#navbar").append(`<div class="topnav">
+						<h1 class="nav_title">RIT Player Piano</h1>
 						<a href="index.html">Home</a>
 						<a href="records.html">Records</a>
 						<a href="contact.html">Contact</a>
