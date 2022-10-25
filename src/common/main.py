9f9a565e287214343b60e427d16b6d5aee06da4e
DavidTheFighter <dallen.19@hotmail.com>
2022-10-24 22:58:47 -0400

    Added device selection + rtmidi as backend

--- src/common/main.py
+++ src/common/main.py
@@ -1,5 +1,6 @@
 from src.common.midi_event import MidiEvent
 from src.mixing.mixing import Mixing
+from src.output_queue.output_queue import OutputQueue
 from src.common.shared_queues import SharedQueues
 import mido
 
@@ -13,11 +14,19 @@ class Main:
         self.create_queues()
         print("Creating Mixing Subsystem")
         self.create_mixing()
-        print("Sanity Check Pass: "+str(self.mixing.is_even(2)))
-        input("Press Enter")
+        print("Creating Output Subsystem")
+        self.create_output()
+
+        mido.set_backend("mido.backends.rtmidi")
+        print("Using Mido backend: {}".format(mido.backend))
+
+        self.output.select_device()
 
     def create_mixing(self):
         self.mixing = Mixing()
+
+    def create_output(self):
+        self.output = OutputQueue()
 
     def create_queues(self):
         self.shared_queues = SharedQueues()
--- src/output_queue/output_queue.py
+++ src/output_queue/output_queue.py
@@ -1,6 +1,27 @@
 import mido
+import time
 
 class OutputQueue:
+    def __init__(self):
+        self._open_port = None
+
+    # Selects the output device to send MIDI to. If `name` is None then the system default is used
+    def select_device(self, name=None):
+        if not name == None and not name in mido.get_output_names():
+            raise Exception('"{}" does not match any of the available devices'.format(name))
+
+        self._open_port = mido.open_output(name)
+
+        print('Switched output device to "{}"'.format(self._open_port.name))
+
+    def check_queue(self, queue):
+        now = time.time()
+        
+        while not queue.empty() and now >= queue.queue[0].timestamp:
+            midiEvent = queue.get()
+
+            self._open_port.send(midiEvent.event)
+
     # Test fucntions for setting up unit test infra. TODO: Remove in sprint 2.
     def is_even(self, number):
         if number % 2 == 0:
