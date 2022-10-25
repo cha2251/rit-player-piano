9463cef69d70942a99477e693b42afc6f366efc1
Carter Aubrey <cha2251@rit.edu>
2022-10-24 21:59:13 -0400

    Code Review Fixes

--- src/common/midi_event.py
+++ src/common/midi_event.py
@@ -5,4 +5,4 @@ import mido
 @dataclass(order=True)
 class MidiEvent:
     event: mido.Message=field(compare=False) # Don't use this when sorting in a priority queue
-    timestamp: int # TODO: Better time class?
+    timestamp: float # Time.time() returns seconds as float
