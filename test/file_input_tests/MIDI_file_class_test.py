412c7312261b056cc44e2a1386185b43696aebfc
Michael Samodurov <43452057+mas8074@users.noreply.github.com>
2022-10-26 21:00:49 -0400

    added midi files folder and sample file

--- test/file_input_tests/MIDI_file_class_test.py
+++ test/file_input_tests/MIDI_file_class_test.py
@@ -4,11 +4,12 @@ import src.file_input.MIDI_file_class as MIDI_FC
 class TestParse:
 
     def test_parse_midi_file(self):
-        val = MIDI_FC.parse_midi_file('song.mid')
+        val = MIDI_FC.parse_midi_file('..\MIDI_Files\MIDI_sample.mid')
 
         expected = 0
 
         actual = val
+
 
         return actual is expected
 
