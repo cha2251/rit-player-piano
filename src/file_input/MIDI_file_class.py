0fdaedb6ff53f4dbbc63e904f0ec233c589e7dc1
Michael Samodurov <43452057+mas8074@users.noreply.github.com>
2022-10-27 20:42:25 -0400

    midi files are parsed into a list

--- src/file_input/MIDI_file_class.py
+++ src/file_input/MIDI_file_class.py
@@ -1,12 +1,25 @@
 """
 Created by Michael Samodurov 10/22/2022
 """
-from operator import contains
+
 import mido
 
 def parse_midi_file(file_name, track_string):
-    file_queue = []
+    """Parse the designated MIDI file and retrieve the selected track.
+    TODO: implement input handling
+
+    Args:
+        file_name (str): The name of the MIDI file to open.
+        track_string (str): The name of the track in the MIDI file to retrieve.
+
+    Returns:
+        list: A list of mido message objects
+    """
+    track_messages = []
     # open midi file
+    if not is_file_string_valid(file_name):
+        return []
+
     file_location = 'MIDI_Files/{}'.format(file_name)
     mid_fi = mido.MidiFile(file_location)
 
@@ -17,11 +30,19 @@ def parse_midi_file(file_name, track_string):
         # iterate over messages in each track
         if track.name == track_string:
             for msg in track:
+                track_messages.append(msg)
                 print(msg.dict())
             
+    return track_messages
 
+def is_file_string_valid(file_string):
 
-    return file_queue
+    is_valid = False
+    if file_string != '':
+        is_valid = True
+    
+    return is_valid
+
 
 
 class MIDIFileObject:
@@ -33,9 +54,12 @@ class MIDIFileObject:
     it is redundant with the mido object.
     """
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, track_string):
+
         self.file_name = file_name
+        self.track_name = track_string
         self.curr_pos = 0
+        self.message_queue = parse_midi_file(file_name, track_string)
 
 
     def __str__(self):
--- test/file_input_tests/MIDI_file_class_test.py
+++ test/file_input_tests/MIDI_file_class_test.py
@@ -4,12 +4,25 @@ import src.file_input.MIDI_file_class as MIDI_FC
 class TestParse:
 
     def test_parse_midi_file(self):
+        """ This is the test to ensure the function returns the correct list of midi messages.
+        TODO: complete this test
+        """
         val = MIDI_FC.parse_midi_file('MIDI_sample.mid', 'Piano')
 
-        expected = 0
+        expected = 100
 
         actual = val
 
 
-        return actual is expected
+        #assert actual == expected
+
+
+    def test_parse_midi_file_is_null(self):
+        """ This test is to ensure the function returns null if no filename is given.
+        """
+        val = MIDI_FC.parse_midi_file('','Piano')
+        expected = []
+        actual = val
+
+        assert actual == expected
 
