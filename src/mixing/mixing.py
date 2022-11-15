9b424234c92219b0dfd7561274067ba13b086423
chaub2251 <cha2251@g.rit.edu>
2022-11-15 18:15:13 -0500

    Parital note tracking for pause

--- src/mixing/mixing.py
+++ src/mixing/mixing.py
@@ -4,6 +4,7 @@ from threading import Thread
 import time
 from src.common.midi_event import MidiEvent
 from src.common.shared_queues import SharedQueues
+import mido
 
 class Mixing(Thread):
     class State():
@@ -18,6 +19,8 @@ class Mixing(Thread):
     active = False
     state = State.STOP
     pause_time = 0
+
+    paused_notes = {}
 
     def __init__(self, shared_queues:SharedQueues):
         Thread.__init__(self)
@@ -38,6 +41,12 @@ class Mixing(Thread):
         self.pause_time= time.time()
         with self.mixed_output_queue.mutex: #Thread safe
             self.holding_queue = deepcopy(self.mixed_output_queue.queue)
+
+        for note in self.paused_notes.keys():
+            if(self.paused_notes[note]=='note_on'):
+                self.mixed_output_queue.put(
+                        MidiEvent(mido.Message('note_off', note, velocity=0), time.time()))
+
         self.mixed_output_queue.queue.clear()
     
     def unpause(self):
@@ -45,6 +54,12 @@ class Mixing(Thread):
         offset_time = time.time() - self.pause_time
         for event in self.holding_queue:
             self.mixed_output_queue.put(MidiEvent(event.event,event.timestamp+offset_time))
+        
+        for note in self.paused_notes.keys():
+            if(self.paused_notes[note]=='note_off'):
+                self.mixed_output_queue.put(
+                        MidiEvent(mido.Message('note_on', note, velocity=120), time.time()))
+
         self.holding_queue.clear()
     
     def main_loop(self):
@@ -58,6 +73,7 @@ class Mixing(Thread):
             if(self.state == self.State.PLAY):
                 try:
                     event = self.file_input_queue.get(timeout=.005)
+                    self.paused_notes.update({event.event.note:event.event.type})
                     self.mixed_output_queue.put(event)
                 except queue.Empty:
                     pass # Expected if we dont have anything in the queue
