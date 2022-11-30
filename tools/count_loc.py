b4a9600cbb95712be5b37cad141fcc97c7090a75
David Allen <19dallen@gmail.com>
2022-11-30 18:41:06 -0500

    Updated the count_loc script to include more directories (#19)

--- tools/count_loc.py
+++ tools/count_loc.py
@@ -2,7 +2,7 @@
 
 import os
 
-SEARCH_DIRECTORIES = ['page', 'tools'] # What directories to recursively search
+SEARCH_DIRECTORIES = ['page', 'tools', 'src', 'test'] # What directories to recursively search
 CODE_FILE_WHITELIST = ['.py', '.js', '.html', '.css']
 
 def getNumberOfLinesInFile(filepath):
@@ -17,6 +17,8 @@ def countLinesOfCodeInRoot(root):
                 filepath = os.path.join(path, filename)
                 linesOfCode = getNumberOfLinesInFile(filepath)
 
+                print("\"{}\" has {} lines".format(filepath, linesOfCode))
+
                 totalLinesOfCode += linesOfCode
 
     return totalLinesOfCode
@@ -28,7 +30,7 @@ def countLinesOfCode():
         searchDir = os.path.join('..', dir)
         totalLinesOfCode += countLinesOfCodeInRoot(searchDir)
 
-    print('Total lines of code: {} ({} kLOC)'.format(totalLinesOfCode, totalLinesOfCode // 1000))
+    print('\nTotal lines of code: {} ({:.2f} kLOC)'.format(totalLinesOfCode, totalLinesOfCode / 1000))
 
 if __name__ == '__main__':
     countLinesOfCode()
