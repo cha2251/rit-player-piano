e56b0a9636df67b48239048c395a80855fafa27b
npawl <nrp4309@g.rit.edu>
2022-12-19 17:54:24 -0500

    documentation and small fixes to button_input.py added

--- src/button_input/button_input.py
+++ src/button_input/button_input.py
@@ -16,6 +16,12 @@ class ButtonInput(Thread):
     default = {'q': 55, 'w': 56, 'e': 57, 'r': 58, 't': 59,
                'y': 60, 'u': 61, 'i': 62, 'o': 63, 'p': 64}
 
+    """ Sets initial values of Thread
+    button_input_queue: global queue responsible for carrying midi events to mixing subsystem
+    keyMap: dictionary of key mappings for input interpretation, set to default if not given
+    
+    a keyboard listener from pynput is also created to detect key presses.
+    """
     def __init__(self, button_input_queue, keyMap=None):
         Thread.__init__(self)
         self.button_input_queue = button_input_queue
@@ -25,30 +31,45 @@ class ButtonInput(Thread):
             self.keyMap = keyMap
         self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
 
+    # Updates the keymap with a new set of mappings
     def changeMap(self, keyMap):
         self.keyMap = keyMap
 
+    # Updates a single key's mapping or adds a new key mapping if the key lacks a map
     def changeKey(self, key, note):
         self.keyMap[key] = note
 
+    # Enables keyboard listener while thread is active
     def run(self):
         self.active = True
         self.listener.start()
         self.listener.join()
         while self.active:
             continue
+        self.listener.stop()
 
+    # Reacts to key presses and sends a midi event if the key is mapped
     def on_press(self, key):
-        if key.char in self.keyMap.keys():
-            note = self.keyMap[key.char]
+        try:
+            k = key.char
+        except:
+            k = key.name
+        if k in self.keyMap.keys():
+            note = self.keyMap[k]
             self.button_input_queue.put(
                 MidiEvent(mido.Message('note_on', note=note, velocity=120), time.time()))
 
+    # Reacts to key releases and sends a midi event if the key is mapped
     def on_release(self, key):
-        if key.char in self.keyMap.keys():
-            note = self.keyMap[key.char]
+        try:
+            k = key.char
+        except:
+            k = key.name
+        if k in self.keyMap.keys():
+            note = self.keyMap[k]
             self.button_input_queue.put(
                 MidiEvent(mido.Message('note_off', note=note, velocity=120), time.time()))
 
+    # Disables active boolean to cancel runtime loop
     def deactivate(self):
         self.active = False
