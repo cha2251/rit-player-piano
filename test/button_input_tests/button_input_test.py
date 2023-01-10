ee1f3a7fed532e75fb44e011ccecd2d2cb64fecb
npawl <nrp4309@g.rit.edu>
2023-01-09 23:16:40 -0500

    button input tests created.

--- test/button_input_tests/button_input_test.py
+++ test/button_input_tests/button_input_test.py
@@ -1,49 +1,72 @@
+import pytest
+from pynput import keyboard
+
 from src.button_input.button_input import ButtonInput
+from src.common.shared_queues import SharedQueues
+from src.common.midi_event import MidiEvent
 
-class TestIsEven:
-    def test_even(self):
-        component = ButtonInput()
-        evenVal = 2
 
-        actual = component.is_even(evenVal)
-        expected = True
-        
-        assert actual is expected
+class TestCreate:
+    def test_create_button_input(self):
+        testQueues = SharedQueues
+        testQueues.create_queues(testQueues)
+        component = ButtonInput(testQueues.button_input_queue)
+        testDict = {'q': 55, 'w': 56, 'e': 57, 'r': 58, 't': 59,
+                    'y': 60, 'u': 61, 'i': 62, 'o': 63, 'p': 64}
 
-    def test_odd(self):
-        component = ButtonInput()
-        evenVal = 1
-        
-        actual = component.is_even(evenVal)
+        expected = testDict
+        actual = component.keyMap
+
+        assert component.button_input_queue is testQueues.button_input_queue
+        assert expected == actual
+
+    def test_dict_preset(self):
+        testQueues = SharedQueues
+        testQueues.create_queues(testQueues)
+        testDict = {'q': 1, 'w': 2}
+        component = ButtonInput(testQueues.button_input_queue, testDict)
+
+        expected = testDict
+        actual = component.keyMap
+
+        assert actual == expected
+
+
+class TestDeactivate:
+    def test_set_false(self):
+        testQueues = SharedQueues
+        testQueues.create_queues(testQueues)
+        component = ButtonInput(testQueues.button_input_queue)
+
+        component.active = True
+        component.deactivate()
+
         expected = False
-        
-        assert actual is expected
+        actual = component.active
 
-class TestInRange:
-    def test_lower_bound(self):
-        component = ButtonInput()
-        lower_val = 3
+        assert actual == expected
 
-        actual = component.in_range(lower_val)
-        expected = False
-        
-        assert actual is expected
-    
-    def test_upper_bound(self):
-        component = ButtonInput()
-        upper_val = 8
 
-        actual = component.in_range(upper_val)
-        expected = False
-        
-        assert actual is expected
-    
-    def test_within_bounds(self):
-        component = ButtonInput()
-        within_range_val = 5
+class TestRun:
+    @pytest.mark.timeout(0.5)
+    def test_exits(self):
+        testQueues = SharedQueues
+        testQueues.create_queues(testQueues)
+        component = ButtonInput(testQueues.button_input_queue)
 
-        actual = component.in_range(within_range_val)
-        expected = True
-        
-        assert actual is expected
+        component.start()
 
+        component.deactivate()
+
+    @pytest.mark.timeout(1)
+    def test_detect_input(self):
+        testQueues = SharedQueues
+        testQueues.create_queues(testQueues)
+        component = ButtonInput(testQueues.button_input_queue)
+
+        component.start()
+        component.on_press(keyboard.KeyCode.from_char('q'))
+        component.on_release(keyboard.KeyCode.from_char('q'))
+        component.deactivate()
+
+        assert component.button_input_queue.empty() is False
