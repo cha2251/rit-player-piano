87c0236065e89e54407fc44bad026ab1b8bd893c
dsp9049 <43452044+dsp9049@users.noreply.github.com>
2023-01-24 17:18:07 -0500

    Stubbed out ui unit testing

--- test/user_interface_tests/home_page_test.py
+++ test/user_interface_tests/home_page_test.py
@@ -1,49 +1,6 @@
 from src.user_interface.home_page import HomePage
 
 class TestIsEven:
-    def test_even(self):
-        component = HomePage()
-        evenVal = 2
 
-        actual = component.is_even(evenVal)
-        expected = True
-        
-        assert actual is expected
-
-    def test_odd(self):
-        component = HomePage()
-        evenVal = 1
-        
-        actual = component.is_even(evenVal)
-        expected = False
-        
-        assert actual is expected
-
-class TestInRange:
-    def test_lower_bound(self):
-        component = HomePage()
-        lower_val = 3
-
-        actual = component.in_range(lower_val)
-        expected = False
-        
-        assert actual is expected
-    
-    def test_upper_bound(self):
-        component = HomePage()
-        upper_val = 8
-
-        actual = component.in_range(upper_val)
-        expected = False
-        
-        assert actual is expected
-    
-    def test_within_bounds(self):
-        component = HomePage()
-        within_range_val = 5
-
-        actual = component.in_range(within_range_val)
-        expected = True
-        
-        assert actual is expected
-
+    def test_default(self):
+        actual = 1 == 1
