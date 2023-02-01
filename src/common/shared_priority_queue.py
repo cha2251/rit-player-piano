9b4f6dfee4b99c222bf1b19abd7be93d15588e54
chaub2251 <cha2251@g.rit.edu>
2023-01-31 21:27:13 -0500

    Queue lock to clean up timing

--- src/common/shared_priority_queue.py
+++ src/common/shared_priority_queue.py
@@ -2,9 +2,12 @@ from copy import deepcopy
 import queue
 from heapq import heappush, heappop
 from multiprocessing.managers import SyncManager
+from threading import Lock
 
 class PeekingPriorityQueue(queue.Queue):
     '''Variant of a PriorityQueue that actually has a peek() function.'''
+
+    accessLock = Lock() #Needed for non built-ins
 
     def _init(self, maxsize):
         self.queue = []
@@ -13,19 +16,21 @@ class PeekingPriorityQueue(queue.Queue):
         return len(self.queue)
 
     def _put(self, item):
-        heappush(self.queue, item)
+        with self.accessLock:
+            heappush(self.queue, item)
 
     def _get(self):
-        return heappop(self.queue)
+        with self.accessLock:
+            return heappop(self.queue)
 
     def peek(self):
         return self.queue[0]
 
     def get_and_clear_queue(self):
-        val = deepcopy(self.queue)
-        self.queue.clear()
-        return val
-
+        with self.accessLock:
+            val = deepcopy(self.queue)
+            self.queue.clear()
+            return val
 
 class SharedQueueSyncManager(SyncManager):
     '''Manages the synchronization of Python objects in between processes'''
