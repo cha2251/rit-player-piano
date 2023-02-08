6c580b0522f9d6f77bc7a97f6b9c13af72647efe
mas8074 <43452057+mas8074@users.noreply.github.com>
2023-02-08 09:09:13 -0500

    added margins and time label

--- src/user_interface/playing_page.py
+++ src/user_interface/playing_page.py
@@ -22,9 +22,11 @@ class PlayingPage(QWidget):
         self.pbar_location = 0;
 
         self.progress = QProgressBar(self)
-        self.progress.setGeometry(200, 100, 200, 30) #not working...
-        self.progress.setAlignment(Qt.AlignCenter)
-        self.progress.setFormat("")
+        self.progress.setGeometry(200, 100, 200, 30)
+        #self.progress.setAlignment(Qt.AlignRight)
+        #self.progress.setFormat("")
+        self.progress_label = QLabel(self)
+        self.progress_label.setText("0:00")
         
 
         playButton = QToolButton()
@@ -66,8 +68,9 @@ class PlayingPage(QWidget):
         ############################################################
         song_hbox = QHBoxLayout()
         song_hbox.setAlignment(Qt.AlignCenter)
-        song_hbox.setContentMargins(100,100,100,100)
+        song_hbox.setContentsMargins(500,0,500,0) # setContentsMargin(left, top, right, bottom)
         song_hbox.addWidget(self.progress)
+        song_hbox.addWidget(self.progress_label)
 
 
         vbox = QVBoxLayout(self)
@@ -113,9 +116,12 @@ class PlayingPage(QWidget):
         self.setWindowTitle("Player Piano: " + song)
 
     def progress_action(self):
-        for i in range(100):
-            time.sleep(0.05)
-            self.progress.setValue(i)
+        # for i in range(101):
+        #     time.sleep(0.05)
+        #     self.progress.setValue(i)
+            #self.progress.setFormat(f'0:{i}')
+        message_delta_time = 1
+        self.progress_label.setText(f'0:{message_delta_time}')
             
     def update_song_progress(self):
         """
@@ -123,6 +129,8 @@ class PlayingPage(QWidget):
         comes in and will update the song progress bar.
         """
         self.progress.setValue(self.pbar_location)
+
+        
 
             
 
