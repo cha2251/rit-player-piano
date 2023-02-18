f6cb6514530c65be87347e9d2e31f35abf991c2a
cha2251 <cha2251@g.rit.edu>
2023-02-18 18:07:08 -0500

    Local comm functions

--- src/communication/local_comm_system.py
+++ src/communication/local_comm_system.py
@@ -1,12 +1,14 @@
-from queue import Queue
+import queue
 from threading import Thread
-from src.communication.messages import MessageType
+import time
+from src.communication.messages import Message, MessageType
 
 
 class LocalCommSystem(Thread):
     handler_map = {MessageType : []} # Map of message types with list of function calls
-    input_queue : Queue
-    output_queue : Queue
+    input_queue : queue.Queue
+    output_queue : queue.Queue
+    active = False
 
     # Overwrite new to only create an instance of this class
     # if there is not one already. If there is an instance
@@ -15,3 +17,29 @@ class LocalCommSystem(Thread):
         if not hasattr(cls, 'instance'):
             cls.instance = super(LocalCommSystem, cls).__new__(cls)
         return cls.instance
+    
+    def send(self, message : Message):
+        self.output_queue.put(message)
+    
+    def registerListner(self, type : MessageType, function):
+        if type in self.handler_map.keys():
+            self.handler_map[type].append(function)
+        else:
+            self.handler_map[type] = [function]
+
+    def callHandlers(self, message : Message):
+        if type in self.handler_map.keys():
+            for function in self.handler_map[message.type]:
+                function(message)
+    
+    def checkMessages(self):
+        while self.active:
+            try:
+                message = self.input_queue.get_nowait()
+                self.callHandlers(message)
+                time.sleep(0)
+            except queue.Empty:
+                pass # Expected if we dont have anything in the queue
+    
+    def deactivate(self):
+        self.active = False
