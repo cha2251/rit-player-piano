b2941cc90068e4bafc0188255382a17ca3cbd0a1
cha2251 <cha2251@g.rit.edu>
2023-03-03 17:03:04 -0500

    Keybaord and controller send midi events

--- src/button_input/button_input.py
+++ src/button_input/button_input.py
@@ -3,6 +3,7 @@ import time
 from threading import Thread
 from pynput import keyboard
 import mido
+from src.button_input.controller import ControllerButton, XboxController
 
 from src.common.midi_event import MidiEvent
 
@@ -10,8 +11,9 @@ from src.common.midi_event import MidiEvent
 class ButtonInput:
     button_input_queue: queue.Queue
     keyMap = dict
-    default = {'q': 55, 'w': 56, 'e': 57, 'r': 58, 't': 59,
-               'y': 60, 'u': 61, 'i': 62, 'o': 63, 'p': 64}
+    default = {'q': [53,54,55], 'w': [56], 'e': [57], 'r': [58], 't': [59],
+               'y': [60], 'u': [61], 'i': [62], 'o': [63], 'p': [64],
+               ControllerButton.A: [65], ControllerButton.B: [66, 68, 70]}
 
     """ Sets initial values of Thread
     button_input_queue: global queue responsible for carrying midi events to mixing subsystem
@@ -25,23 +27,28 @@ class ButtonInput:
             self.keyMap = self.default
         else:
             self.keyMap = keyMap
-        self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        
+        self.keyboard_listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        self.keyboard_listener.start()
+        self.controller = XboxController(self.add_controller_note)
 
     # Updates the keymap with a new set of mappings
     def change_map(self, keyMap):
         self.keyMap = keyMap
 
     # Updates a single key's mapping or adds a new key mapping if the key lacks a map
-    def change_key(self, key, note):
-        self.keyMap[key] = note
+    def change_key(self, key, notes):
+        # Check if chord or single note
+        if isinstance(notes, list):
+            self.keyMap[key] = notes
+        else:
+            self.keyMap[key] = [notes] # Make single note a chord
 
-    # Removes an unneeded key from the key map
     def delete_key(self, key):
         self.keyMap.pop(key)
 
-    # Enables keyboard listener while thread is active
     def run(self):
-        self.listener.start()
+        self.keyboard_listener.start()
 
     # Reacts to key presses and sends a midi event if the key is mapped
     def on_press(self, key):
@@ -50,9 +57,9 @@ class ButtonInput:
         except:
             k = key.name
         if k in self.keyMap.keys():
-            note = self.keyMap[k]
-            self.button_input_queue.put(
-                MidiEvent(mido.Message('note_on', note=note, velocity=120), time.time()))
+            for note in self.get_notes(k):
+                self.button_input_queue.put(
+                    MidiEvent(mido.Message('note_on', note=note, velocity=120), 0))
 
     # Reacts to key releases and sends a midi event if the key is mapped
     def on_release(self, key):
@@ -61,10 +68,29 @@ class ButtonInput:
         except:
             k = key.name
         if k in self.keyMap.keys():
-            note = self.keyMap[k]
-            self.button_input_queue.put(
-                MidiEvent(mido.Message('note_off', note=note, velocity=120), time.time()))
+            for note in self.get_notes(k):
+                self.button_input_queue.put(
+                    MidiEvent(mido.Message('note_off', note=note, velocity=120), 0))
+            
+    # Adds a note when a button on the controller is pressed
+    def add_controller_note(self, button : ControllerButton, state):
+        if state == 1:
+            for note in self.get_notes(button):
+                print(note)
+                self.button_input_queue.put(
+                    MidiEvent(mido.Message('note_on', note=note, velocity=120), 0))
+        else:
+            for note in self.get_notes(button):
+                self.button_input_queue.put(
+                    MidiEvent(mido.Message('note_off', note=note, velocity=0), 0))
+            
+    def get_notes(self, button : ControllerButton):
+        try:
+            return self.keyMap[button]
+        except KeyError:
+            return []
 
     # Stops listener thread
     def deactivate(self):
-        self.listener.stop()
+        self.controller.deactivate()
+        self.keyboard_listener.stop()
---
+++ src/button_input/controller.py
@@ -0,0 +1,67 @@
+import threading
+from inputs import get_gamepad
+from enum import Enum
+
+class ControllerButton(Enum):
+    LeftJoystickY = 1
+    LeftJoystickX = 2
+    RightJoystickY = 3
+    RightJoystickX = 4
+    LeftTrigger = 5
+    RightTrigger = 6
+    LeftBumper = 7
+    RightBumper = 8
+    A = 9
+    X = 10
+    Y = 11
+    B = 12
+    LeftThumb = 13
+    RightThumb = 14
+    Back = 15
+    Start = 16
+    LeftDPad = 17
+    RightDPad = 18
+    UpDPad = 19
+    DownDPad = 20
+
+class XboxController:
+    def __init__(self, add_controller_note):
+        # Map of event codes to controller buttons
+        self.event_button_map = {
+            'ABS_Y': ControllerButton.LeftJoystickY,
+            'ABS_X': ControllerButton.LeftJoystickX,
+            'ABS_RY': ControllerButton.RightJoystickY,
+            'ABS_RX': ControllerButton.RightJoystickX,
+            'ABS_Z': ControllerButton.LeftTrigger,
+            'ABS_RZ': ControllerButton.RightTrigger,
+            'BTN_TL': ControllerButton.LeftBumper,
+            'BTN_TR': ControllerButton.RightBumper,
+            'BTN_SOUTH': ControllerButton.A,
+            'BTN_EAST': ControllerButton.B,
+            'BTN_NORTH': ControllerButton.Y,
+            'BTN_WEST': ControllerButton.X,
+            'BTN_THUMBL': ControllerButton.LeftThumb,
+            'BTN_THUMBR': ControllerButton.RightThumb,
+            'BTN_SELECT': ControllerButton.Back,
+            'BTN_START': ControllerButton.Start,
+            'BTN_DPAD_LEFT': ControllerButton.LeftDPad,
+            'BTN_DPAD_RIGHT': ControllerButton.RightDPad,
+            'BTN_DPAD_UP': ControllerButton.UpDPad,
+            'BTN_DPAD_DOWN': ControllerButton.DownDPad
+        }
+
+        self.add_controller_note = add_controller_note
+        self.active = True
+        self.controller_listener = threading.Thread(target=self.listener, args=())
+        self.controller_listener.start()
+
+    def listener(self):
+        while self.active:
+            events = get_gamepad()
+            for event in events:
+                if event.code in self.event_button_map:
+                    self.add_controller_note(self.event_button_map[event.code], event.state)
+
+    def deactivate(self):
+        self.active = False
+    
