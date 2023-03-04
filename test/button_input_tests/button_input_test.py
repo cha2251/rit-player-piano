35633a07874a0f1cd014373d35c627571c42856c
Carter Aubrey <54385736+cha2251@users.noreply.github.com>
2023-03-04 17:39:59 -0500

    Controller Input (#48)
    * Keybaord and controller send midi events
    
    * Controller inputs play notes
    
    * Button test fixes
    
    * PR fixes
    
    * fixed requirements
    
    * Rename callback
    
    * Removed matplotlib
    
    * Pass tests on CI
    
    * Pass tests on CI please
    
    * Pass tests on CI pretty please

--- test/button_input_tests/button_input_test.py
+++ test/button_input_tests/button_input_test.py
@@ -3,20 +3,25 @@ import pytest
 from pynput import keyboard
 
 from src.button_input.button_input import ButtonInput
+from src.button_input.controller import ControllerButton
 
 
 class TestCreate:
+    @pytest.mark.timeout(5)
     def test_create_button_input(self):
         testQueue = Queue()
         component = ButtonInput(testQueue)
-        testDict = {'q': 55, 'w': 56, 'e': 57, 'r': 58, 't': 59,
-                    'y': 60, 'u': 61, 'i': 62, 'o': 63, 'p': 64}
+        testDict = {'q': [53,54,55], 'w': [56], 'e': [57], 'r': [58], 't': [59],
+               'y': [60], 'u': [61], 'i': [62], 'o': [63], 'p': [64],
+               ControllerButton.A: [65], ControllerButton.B: [66, 68, 70]}
 
         expected = testDict
         actual = component.keyMap
 
         assert component.button_input_queue is testQueue
         assert expected == actual
+
+        component.deactivate()
 
     def test_dict_preset(self):
         testQueue = Queue()
@@ -28,12 +33,15 @@ class TestCreate:
 
         assert actual == expected
 
+        component.deactivate()
+
 
 class TestModify:
+    @pytest.mark.timeout(1)
     def test_change_map(self):
         testQueue = Queue()
         component = ButtonInput(testQueue)
-        testDict = {'q': 1, 'w': 2}
+        testDict = {'q': [1], 'w': 2}
 
         assert component.keyMap != testDict
 
@@ -44,14 +52,14 @@ class TestModify:
 
         assert actual == expected
 
+        component.deactivate()
+
 
 class TestRun:
     @pytest.mark.timeout(1)
     def test_exits(self):
         testQueue = Queue()
         component = ButtonInput(testQueue)
-
-        component.run()
 
         component.deactivate()
 
@@ -60,7 +68,6 @@ class TestRun:
         testQueue = Queue()
         component = ButtonInput(testQueue)
 
-        component.run()
         component.on_press(keyboard.KeyCode.from_char('q'))
         component.on_release(keyboard.KeyCode.from_char('q'))
         component.deactivate()
