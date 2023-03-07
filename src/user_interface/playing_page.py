4ff03c9185a031eb6083275e4cd871ececfdee5d
dsp9049 <43452044+dsp9049@users.noreply.github.com>
2023-03-07 00:39:50 -0500

    Update src/user_interface/playing_page.py

--- src/user_interface/playing_page.py
+++ src/user_interface/playing_page.py
@@ -149,7 +149,3 @@ class PlayingPage(QWidget):
         self.progress.setValue(self.pbar_location)
 
 
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     ex = PlayingPage()
-#     sys.exit(app.exec_())
