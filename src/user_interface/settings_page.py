14edd828983dda0cf2f2643db5a24c6149d26b95
dsp9049 <43452044+dsp9049@users.noreply.github.com>
2023-03-22 19:40:43 -0400

    right stick typo

--- src/user_interface/settings_page.py
+++ src/user_interface/settings_page.py
@@ -191,7 +191,7 @@ class SettingsPage(QWidget):
         self.RS.setIcon(QIcon(os.path.join(os.path.dirname(__file__), "..", "..", "UI_Images", "settings", "button-RS.svg")))
         self.RS.setStyleSheet(tool)
         self.RS.setIconSize(QSize(60, 50))
-        iconsList.addWidget(self.LS)
+        iconsList.addWidget(self.RS)
 
         vbox.addLayout(iconsList)
 
