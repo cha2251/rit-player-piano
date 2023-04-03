dad1a4868b4b6ee9a777e0080156b3a7bd5d54fd
dsp9049 <43452044+dsp9049@users.noreply.github.com>
2023-04-03 15:46:40 -0400

    add configuration to playing page

--- src/user_interface/configPopup.py
+++ src/user_interface/configPopup.py
@@ -1,30 +1,49 @@
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import QDialog, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QStackedLayout
 from src.user_interface.iconsList import IconsList
 from src.user_interface.piano import Piano
+from PyQt5.QtCore import Qt, QRect
 
 
 class ConfigPopup(QDialog):
 
     def __init__(self):
         super().__init__()
+        stacked = QStackedLayout()
+        gray = QRect()
         layout = QVBoxLayout()
-        #uncomment this for production
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setGeometry(100, 100, 500, 200)
         layout.addLayout(IconsList())
         layout.addLayout(Piano())
+
+        # confirm / cancel buttons
+
+        hbox = QHBoxLayout()
+        hbox.setAlignment(Qt.AlignRight)
+        cancel = QPushButton("cancel")
+        cancel.setStyleSheet("""
+            max-width: 5em;
+        """)
+        cancel.clicked.connect(self.close)
+        confirm = QPushButton("Confirm")
+        confirm.setStyleSheet("""
+            max-width: 5em:
+        """)
+        hbox.addWidget(cancel)
+        hbox.addWidget(confirm)
+        layout.addLayout(hbox)
         self.setLayout(layout)
-        #self.addLayout(Piano())
+        # self.addLayout(Piano())
 
         self.show()
 
     def showEvent(self, event):
-        #geom = self.frameGeometry()
-        #geom.moveCenter(QtGui.QCursor.pos())
-        #self.setGeometry(geom)
+        # geom = self.frameGeometry()
+        # geom.moveCenter(QtGui.QCursor.pos())
+        # self.setGeometry(geom)
         super(ConfigPopup, self).showEvent(event)
-        #self.showEvent(event)
+        # self.showEvent(event)
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Escape:
@@ -32,3 +51,6 @@ class ConfigPopup(QDialog):
             event.accept()
         else:
             super(ConfigPopup, self).keyPressEvent(event)
+
+    def save(self):
+        pass
